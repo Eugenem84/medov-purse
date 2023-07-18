7ec0b4ffd477d4f096c63117d151d6cb6a024353
medov 1st comit <medov-foto@mail.ru>
2023-07-18 10:40:44 +0300

    переделал c else if на switch case но не работает должным образом

--- app/purse.php
+++ app/purse.php
@@ -22,40 +22,60 @@ function check_redirection($url)
     curl_setopt($ch, CURLOPT_HEADER, true);
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    if ($httpCode === 301 || $httpCode === 302) {
-        $headers = explode("\n", $response);
-        foreach ($headers as $header) {
-            if (strpos($header, 'Location') !== false) {
-                $rediracted_url = trim(str_replace('Location:', '', $header));
-                array_push($URLs, $rediracted_url);
-                check_redirection($rediracted_url);
-                $resURLs = $URLs;
-                break;
-            }
-        }
-    } else if ($httpCode === 500) {
-        answerMessage("Internal Server Error");
-    } else if ($httpCode === 404) {
-        answerMessage("Not Found");
-    } else if ($httpCode === 403) {
-        answerMessage("Forbidden:");
-    } else if ($httpCode === 400) {
-        answerMessage("Bad Request");
-    } else if ($httpCode === 200) {
-        answerMessage("link is not redirected");
-    } else {
-        answerMessage("i d't no what's going on");
+    switch ($httpCode) {
+        case 301:
+        case 302:
+            $headers = explode("\n", $response);
+            foreach ($headers as $header) {
+                if (strpos($header, "Location") !== false) {
+                    $rediracted_url = trim(str_replace("Location", '', $header));
+                    array_push($URLs, $rediracted_url);
+                    check_redirection($rediracted_url);
+                    $resURLs = $URLs;
+                }
+            } break;
+        case 500: answerMessage("Internal Server Error"); break;
+        case 404: answerMessage("Not Found"); break;
+        case 403: answerMessage("Forbidden"); break;
+        case 400: answerMessage("Bad Request"); break;
+        case 200: answerMessage("Link is not redirected"); break;
+        default: answerMessage("I d't now whats going on"); break;
     }
+
+//    if ($httpCode === 301 || $httpCode === 302) {
+//        $headers = explode("\n", $response);
+//        foreach ($headers as $header) {
+//            if (strpos($header, 'Location') !== false) {
+//                $rediracted_url = trim(str_replace('Location:', '', $header));
+//                array_push($URLs, $rediracted_url);
+//                check_redirection($rediracted_url);
+//                $resURLs = $URLs;
+//                break;
+//            }
+//        }
+//    } else if ($httpCode === 500) {
+//        answerMessage("Internal Server Error");
+//    } else if ($httpCode === 404) {
+//        answerMessage("Not Found");
+//    } else if ($httpCode === 403) {
+//        answerMessage("Forbidden:");
+//    } else if ($httpCode === 400) {
+//        answerMessage("Bad Request");
+//    } else if ($httpCode === 200) {
+//        answerMessage("link is not redirected");
+//    } else {
+//        answerMessage("i d't no what's going on");
+//    }
 }
 
 //прием
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $message = trim($_POST['data']);
     check_redirection($message);
+    if ($resURLs[0] === $message) {
+        answerMessage("link is not redirection");
     if (!empty($resURLs)) {
         answerURL($resURLs);
-    } else if ($resURLs[0] = $message) {
-        answerMessage("link is not redirection");
     } else {
         answerMessage("something else going on");
     }
