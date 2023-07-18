d82e7b823b43978191086ea85f6a4b74fa3aca8e
medov 1st comit <medov-foto@mail.ru>
2023-07-18 12:49:44 +0300

    починил, работает

--- app/purse.php
+++ app/purse.php
@@ -15,12 +15,19 @@ function answerMessage ($answerString) {
 }
 function check_redirection($url)
 {
+    // результирующая глобальная переменная с массивом адресов
     global $resURLs;
+    // массив с адресами сохраняющий значения в рекурсии
     static $URLs = [];
+    // создается обработчик запросов с указаной ссылкой
     $ch = curl_init($url);
+    // опция для обработчика ссылки что бы возвращал результат в виде строки а не выводился напрямую
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //опция для обработчика что бы заголовки ответа возвращал вместе с телом ответа
     curl_setopt($ch, CURLOPT_HEADER, true);
+    // выполнение запроса хттп использую обработчик с урл сохраняет все в response
     $response = curl_exec($ch);
+    // получаем код ответа
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     switch ($httpCode) {
         case 301:
@@ -28,20 +35,30 @@ function check_redirection($url)
             $headers = explode("\n", $response);
             foreach ($headers as $header) {
                 if (strpos($header, "Location") !== false) {
-                    $rediracted_url = trim(str_replace("Location", '', $header));
+                    $rediracted_url = trim(str_replace("Location:", '', $header));
                     array_push($URLs, $rediracted_url);
                     check_redirection($rediracted_url);
-                    $resURLs = $URLs;
                 }
-            } break;
-        case 500: answerMessage("Internal Server Error"); break;
-        case 404: answerMessage("Not Found"); break;
-        case 403: answerMessage("Forbidden"); break;
-        case 400: answerMessage("Bad Request"); break;
-        case 200: answerMessage("Link is not redirected"); break;
-        default: answerMessage("I d't now whats going on"); break;
+            }
+            break;
+        case 500:
+            answerMessage("Internal Server Error");
+            break;
+        case 404:
+            answerMessage("Not Found");
+            break;
+        case 403:
+            answerMessage("Forbidden");
+            break;
+        case 400:
+            answerMessage("Bad Request");
+            break;
+//        case 200:
+//            answerMessage("Link is not redirected");
+//            break;
     }
-
+    $resURLs = $URLs;
+}
 //    if ($httpCode === 301 || $httpCode === 302) {
 //        $headers = explode("\n", $response);
 //        foreach ($headers as $header) {
@@ -66,22 +83,25 @@ function check_redirection($url)
 //    } else {
 //        answerMessage("i d't no what's going on");
 //    }
-}
+
 
 //прием
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $message = trim($_POST['data']);
     check_redirection($message);
-    if ($resURLs[0] === $message) {
-        answerMessage("link is not redirection");
     if (!empty($resURLs)) {
         answerURL($resURLs);
-    } else {
-        answerMessage("something else going on");
+    } else if (!empty($resURLs) && $resURLs[0] === $message) {
+        answerMessage("Link is not redirected");
     }
-} else {
-    answerMessage("data was not keep \n");
 }
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    answerMessage("POST is waiting\n");
-}
+
+//if (!empty($resURLs)) {
+//   answerURL($resURLs);
+//} else if (!empty($resURLs) && $resURLs[0] === $message) {
+//    answerMessage("Link is not redirected");
+//}
+
+//if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+//    answerMessage("POST is waiting\n");
+//}
